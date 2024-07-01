880a80b10cc0f9fca9703eec4eab4ae190a3fbfe
Parazyan_Roman <romaparazyan@gmail.com>
2024-07-01 19:03:46 +0300

    added clerc auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-medium text-sky-700">Hello world</p>
+    <>
+        <p className="text-3xl font-medium text-sky-700">Hello world</p>
+        <Button variant='destructive'>Click me</Button>
+    </>
   );
 }
